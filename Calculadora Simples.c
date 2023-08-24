272352e580f24a193d5cb6b83baafdb1e3ff6455
Christopher Santos <112826117+ochristopherfilipe@users.noreply.github.com>
2023-08-23 22:45:13 -0400

    comented Calculadora Simples.c
    adicionados comentários no código

--- Calculadora Simples.c
+++ Calculadora Simples.c
@@ -1,37 +1,36 @@
 #include <stdio.h>
 
 int main() {
-    char operator;
-    double num1, num2;
+    char operator; // Variável para armazenar o operador (+, -, *, /)
+    double num1, num2; // Variáveis para armazenar os números inseridos pelo usuário
 
     printf("Calculadora Simples\n");
     printf("Digite um operador (+, -, *, /): ");
-    scanf("%c", &operator);
+    scanf("%c", &operator); // Solicita ao usuário para inserir um operador
 
-    printf("Digite dois numeros: ");
-    scanf("%lf %lf", &num1, &num2);
+    printf("Digite dois números: ");
+    scanf("%lf %lf", &num1, &num2); // Solicita ao usuário para inserir dois números
 
     switch (operator) {
         case '+':
-            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2);
+            printf("%.2lf + %.2lf = %.2lf\n", num1, num2, num1 + num2); // Realiza a adição
             break;
         case '-':
-            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2);
+            printf("%.2lf - %.2lf = %.2lf\n", num1, num2, num1 - num2); // Realiza a subtração
             break;
         case '*':
-            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2);
+            printf("%.2lf * %.2lf = %.2lf\n", num1, num2, num1 * num2); // Realiza a multiplicação
             break;
         case '/':
             if (num2 != 0) {
-                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2);
+                printf("%.2lf / %.2lf = %.2lf\n", num1, num2, num1 / num2); // Realiza a divisão (verifica se o divisor não é zero)
             } else {
-                printf("Divisao por zero nao eh permitida.\n");
+                printf("Divisão por zero não é permitida.\n"); // Trata divisão por zero
             }
             break;
         default:
-            printf("Operador invalido.\n");
+            printf("Operador inválido.\n"); // Trata operador inválido
     }
 
     return 0;
 }
-
